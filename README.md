4c4a5ba93a6eabf4b780e568271426e72d75e505
Narendra21s <sivanarendra098@gmail.com>
2024-04-12 07:17:07 +0530

    add server.js

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ## Server Creation >> npm init
+
+## Running sever >> node server.js
+
+## >> npm start (or)
+
+## >> npm run yourScriptName
+
+## npm >> node package manager
--- server.js
+++ server.js
@@ -2,14 +2,19 @@
 
 const http=require("http");
 
-const port=8081;
+ const port=8081; // local port number
 
 http
-   .createServer((req,res) => {
-      res.writeHead(200,{"Content-Type":"text/html"});
-      res.write("<h2>The server creation is done :-)</h2>");
+   .createServer((req,res) => { // call back fun
+    res.writeHead(200,{"Content-Type":"text/html"});
+      res.write("<h2>The server creation is done :-) 7647 </h2>");
       res.end();
    })
-   .listen(port,() => {
+   .listen(port,() => {  // call back fun
       console.log(`NodeJs server started running on ${port}`);
    })
+
+   // https://localhost:8081
+
+   
+
